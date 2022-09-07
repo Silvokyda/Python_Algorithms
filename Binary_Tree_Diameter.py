d6c9c87aaa58e629aad25ad4c5ca2c5a667ca27c
Silvanus Oketch <97881965+Silvokyda@users.noreply.github.com>
2022-09-08 01:37:50 +0300

    Create Binary_Tree_Diameter.py

---
+++ Binary_Tree_Diameter.py
@@ -0,0 +1,36 @@
+'''
+    Binary Tree Diameter:
+    Given a binary tree, output the diameter (length of its longest path,
+    regardless of if the path passes through the root)
+    Time:  O(N)
+    Space: O(N), (Alternate O(D), where D is the depth of binary tree)
+	Last Practiced: 2022-03-23 07:19:27
+'''
+# This is an input class. Do not edit.
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class TreeInfo:
+	def __init__(self, diameter, height):
+		self.diameter = diameter
+		self.height = height
+		
+def binaryTreeDiameter(tree):
+    return getTreeInfo(tree).diameter
+
+def getTreeInfo(tree):
+	if tree is None:
+		return TreeInfo(0,0)
+	leftTreeInfo = getTreeInfo(tree.left)
+	rightTreeInfo = getTreeInfo(tree.right)
+	
+	maxPathOverRoot = leftTreeInfo.height + rightTreeInfo.height
+	maxDiameterSoFar = max(leftTreeInfo.diameter, rightTreeInfo.diameter)
+	currentDiameter = max(maxPathOverRoot, maxDiameterSoFar)
+	currentHeight = 1 + max(leftTreeInfo.height, rightTreeInfo.height)
+	
+	return TreeInfo(currentDiameter, currentHeight)
